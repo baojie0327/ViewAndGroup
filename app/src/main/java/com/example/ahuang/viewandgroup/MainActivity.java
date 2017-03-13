10bdd48e4e8ddb014529f792b7ed0f9182025876
Farmer uncle <bj0327_work@163.com>
2017-03-13 10:47:53 +0800

    recyclview

--- app/src/main/java/com/example/ahuang/viewandgroup/MainActivity.java
+++ app/src/main/java/com/example/ahuang/viewandgroup/MainActivity.java
@@ -3,21 +3,29 @@ package com.example.ahuang.viewandgroup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
+import com.example.ahuang.viewandgroup.adapter.MainAdapter;
 import com.example.ahuang.viewandgroup.viewmeasure.ViewMeasureActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @Bind(R.id.viewMeasure)
-    TextView mViewMeasure;
+
     @Bind(R.id.activity_main)
     LinearLayout mActivityMain;
+    @Bind(R.id.recycleView)
+    RecyclerView mRecycleView;
+    private List<String> list = new ArrayList<>();
+    private MainAdapter mAdapter;
 
 
     @Override
@@ -25,16 +33,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        mViewMeasure.setOnClickListener(this);
+        //  mViewMeasure.setOnClickListener(this);
+        list.add("View的测量");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecycleView.setLayoutManager(layoutManager);
+
+        mAdapter = new MainAdapter(this, list);
+        mRecycleView.setAdapter(mAdapter);
+        mAdapter.setOnItemClickLitener(new MainAdapter.OnItemClickLitener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                switch (position) {
+                    case 0:
+                        Intent intent_measure = new Intent(MainActivity.this, ViewMeasureActivity.class);
+                        startActivity(intent_measure);
+                        break;
+                }
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.viewMeasure:
-                Intent intent_measure = new Intent(MainActivity.this, ViewMeasureActivity.class);
-                startActivity(intent_measure);
-                break;
+//            case R.id.viewMeasure:
+//                Intent intent_measure = new Intent(MainActivity.this, ViewMeasureActivity.class);
+//                startActivity(intent_measure);
+//                break;
         }
     }
 }
--- app/src/main/java/com/example/ahuang/viewandgroup/adapter/MainAdapter.java
+++ app/src/main/java/com/example/ahuang/viewandgroup/adapter/MainAdapter.java
@@ -4,37 +4,83 @@ package com.example.ahuang.viewandgroup.adapter; /**
  */
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.ahuang.viewandgroup.R;
+
+import java.util.List;
 
 /**
  * class description here
+ *
  * @author Borje
  * @version 1.0.0
- * since 2017 03 09
+ *          since 2017 03 09
  */
-public class MainAdapter extends BaseAdapter{
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MyViewHolder> {
 
     private Context mContext;
+    private List<String> list;
+    private OnItemClickLitener mOnItemClickLitener;
+
+    public MainAdapter(Context context, List<String> list) {
+        this.mContext = context;
+        this.list = list;
+    }
+
 
     @Override
-    public int getCount() {
-        return 0;
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_main,
+                parent, false);
+        MyViewHolder viewHolder=new MyViewHolder(view);
+        viewHolder.tv_title=(TextView) view.findViewById(R.id.viewMeasure);
+        viewHolder.ll_listen=(LinearLayout) view.findViewById(R.id.ll_listen);
+        return viewHolder;
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        holder.tv_title.setText(list.get(position));
+        // 如果设置了回调，则设置点击事件
+        if (mOnItemClickLitener != null) {
+            holder.ll_listen.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickLitener.onItemClick(holder.itemView, pos);
+                }
+            });
+
+        }
     }
 
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public int getItemCount() {
+        return list.size();
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        TextView tv_title;
+        LinearLayout ll_listen;
+    }
+
+    public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener) {
+        this.mOnItemClickLitener = mOnItemClickLitener;
+    }
+
+    public interface OnItemClickLitener {
+        void onItemClick(View view, int position);
     }
 }
